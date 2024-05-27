819a4f788c764019a4f3740a737badda612b69f7
Ericsson Gomes <contato@ericssongomes.com>
2024-05-27 14:30:14 -0300

    Adjustment for delete image-files

--- backend/src/services/chars/DeleteCharService.ts
+++ backend/src/services/chars/DeleteCharService.ts
@@ -1,4 +1,6 @@
 import prismaClient from "../../prisma";
+import fs from 'fs';
+import path from 'path'
 
 interface CharIdRequest {
     id: string;
@@ -20,6 +22,17 @@ class DeleteCharService {
                     id: id
                 }
             })
+            if (char.image) {
+
+                const imagePath = path.resolve(__dirname, '..', '..', '..', 'tmp', char.image);
+                fs.unlink(imagePath, (err) => {
+                    if (err) {
+                        console.error(`Erro ao deletar o arquivo de imagem ${char.image}:`, err);
+                    } else {
+                        console.log(`Arquivo de imagem ${char.image} deletado com sucesso`);
+                    }
+                });
+            }
 
             console.log(`Personagem ${id} deletado`)
             return char;
