ba21e5502226a0633132836d74ef1b9aa06fb341
Ericsson Gomes <contato@ericssongomes.com>
2024-06-04 22:11:32 -0300

    update middleware

--- backend/src/middlewares/isAuthenticated.ts
+++ backend/src/middlewares/isAuthenticated.ts
@@ -3,20 +3,17 @@ import { verify } from "jsonwebtoken";
 
 interface PayLoad {
     sub: string;
-
 }
-
-
 export function isAuthenticated(
     req: Request,
     res: Response,
     next: NextFunction
 ) {
-
     //verificar o token e autenticar o usuario
     const authToken = req.headers.authorization;
 
     if (!authToken) {
+        console.log("Middleware acusando que Token não fornecido.")
         return res.status(401).end();
 
     }
@@ -30,15 +27,13 @@ export function isAuthenticated(
             process.env.JWT_SECRET
         ) as PayLoad;
 
-        //aqui recupera o id do token e coloca dentro da variavel user_id dentro do req.
-        const subString = sub.toString();
-        req.user_id = subString;
-
+        //aqui recupera o id do token e coloca dentro da variavel user_id dentro do req.        
+        req.user_id = sub.toString();
         return next();
-
     } catch (err) {
-        console.log('erro no try do middleware', err)
-        return res.status(401).end();
+        console.log('erro no try do middleware. verificação do token com problema: ', err)
+        // return res.status(401).end();
+        return res.status(401).json({ error: "Token inválido" })
     }
 
 
